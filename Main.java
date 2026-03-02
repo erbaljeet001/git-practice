26de63788392f69c596d62864252ef8b58805598
Baljeet Kumar Yadav <abhinandan9935@gmail.com>
2026-03-02 12:30:52 +0530

    java First program

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub 🚀");
+        System.out.println("My name is Baljeet Kumar Yadav");
+        System.out.println("This is my first Java program pushed to GitHub.");
+
+        // Simple calculation
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
